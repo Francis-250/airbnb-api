54b700bfeca5d292953ea1a8567b1d879e6ba32d
Francis-250 <munyankindif0@gmail.com>
2026-04-22 16:32:37 +0200

    protected_the_route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,13 +6,14 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/users.controller";
+import { verifyToken } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
+router.get("/", verifyToken, getAllUsers);
+router.get("/:id", verifyToken, getUserById);
 router.post("/", createUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
