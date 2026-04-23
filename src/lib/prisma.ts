710a2a4a7f8d5974d1255c5081f78090910d11da
Francis-250 <munyankindif0@gmail.com>
2026-04-23 09:55:36 +0200

    fix: tsconfig and prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,9 @@
-import dotenv from "dotenv";
-dotenv.config();
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
+import { Pool } from "pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new pg.Pool({ connectionString });
+const connectionString = process.env.DATABASE_URL as string;
+const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
